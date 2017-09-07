40c4590adb6e2037e7308b2c808fb80c479a54d7
Aristotelis Chantzaras <arisxan@hol.gr>
2017-09-07 22:00:23 +0300

    CameraCaptureStreamer and ScreenCaptureStreamer constructors pass-on stop anonymous "delegate" parameter to base constructor

--- ScreenCaptureStreamer.WinForms/ScreenCaptureStreamer.cs
+++ ScreenCaptureStreamer.WinForms/ScreenCaptureStreamer.cs
@@ -4,13 +4,14 @@
 
 using Chantzaras.Media.Streaming.Mjpeg;
 using Chantzaras.Media.Capture;
+using System;
 
 namespace Chantzaras.Media.Streaming.Screencast
 {
     public class ScreenCaptureStreamer : MjpegStreamer
     {
 
-        public ScreenCaptureStreamer(int width, int height, bool showCursor) : base((ScreenCapture.Snapshots(width, height, showCursor))) //TODO: pass a Func<bool> here for stopping the enumerable (override Stop to set some boolean)
+        public ScreenCaptureStreamer(int width, int height, bool showCursor, Func<bool> stop=null) : base((ScreenCapture.Snapshots(width, height, showCursor, stop)))
         {
         }
 
--- UWP/CameraStreamer.UWP/CameraCaptureStreamer.cs
+++ UWP/CameraStreamer.UWP/CameraCaptureStreamer.cs
@@ -6,13 +6,14 @@ using Windows.UI.Xaml.Controls;
 
 using Chantzaras.Media.Streaming.Mjpeg;
 using Chantzaras.Media.Capture;
+using System;
 
 namespace Chantzaras.Media.Streaming.Cameracast
 {
     public class CameraCaptureStreamer : MjpegStreamer
     {
 
-        public CameraCaptureStreamer(CaptureElement previewControl, int width, int height) : base((CameraCapture.Snapshots(previewControl, width, height))) //TODO: pass a Func<bool> here for stopping the enumerable (override Stop to set some boolean)
+        public CameraCaptureStreamer(CaptureElement previewControl, int width, int height, Func<bool> stop=null) : base((CameraCapture.Snapshots(previewControl, width, height, stop)))
         {
         }
 
